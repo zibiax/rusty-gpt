8f99aafb5b78c2dfae09508b6e25aaa5e228a9de
Martin <martin.evenbom@gmail.com>
2023-04-07 18:32:04 +0200

    struct

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,26 @@ use hyper_tls::HttpsConnector;
 use serde_derive::{Deserialize, Serialize};
 use spinners::{Spinner, Spinners};
 use std::env;
+use std::io::{stdin, stdout, Write};
 
-fn
+#[derive(Deserialize, Debug)]
+struct OAIChoices {
+    text: String,
+    index: u8,
+    logprobs: Otion<u8>,
+    finish_reason: String,
+}
+#[derive(Deserialize, Debug)]
+    struct OAIResponses {
+        id: Option<String>,
+        object: Option<String>,
+        created: Option<u64>,
+        model: Option<String>,
+        choices: Vec<OAIChoices>,
+}
+
+#[derive(Serialize, Debug)
+struct OAIRequest {
+
+fn main () {
+
