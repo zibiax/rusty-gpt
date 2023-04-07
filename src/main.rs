07b4654b32c112bd6369528141b13a711f3a4ce0
Martin <martin.evenbom@gmail.com>
2023-04-07 18:19:47 +0200

    main structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
-}
+use hyper::body::Buf;
+use hyper::{header, Body, Client, Request};
+use hyper_tls::HttpsConnector;
+use serde_derive::{Deserialize, Serialize};
+use spinners::{Spinner, Spinners};
+use std::env;
+
+fn
